61b423304a9bbdba51a6b011e22316306be02c33
Jay <lijiakof@gmail.com>
2018-11-15 16:48:45 +0800

    Complete debugger but need test

--- src/pages/room-night-customer/.query.vue
+++ src/pages/room-night-customer/.query.vue
@@ -18,6 +18,48 @@
             functionName="tokenURI"
             :parameters="[{key: 'tokenId', value: 1}]">
         </trip-debugger>
+        <trip-debugger 
+            title="balanceOf"
+            className="roomNightCustomer"
+            functionName="balanceOf"
+            :parameters="[{key: 'owner', value: '0x262bab6a90aa1741390c4a3ec58855c81d9728e1'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="ownerOf"
+            className="roomNightCustomer"
+            functionName="ownerOf"
+            :parameters="[{key: 'tokenId', value: 1}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="getApproved"
+            className="roomNightCustomer"
+            functionName="getApproved"
+            :parameters="[{key: 'tokenId', value: 1}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="isApprovedForAll"
+            className="roomNightCustomer"
+            functionName="isApprovedForAll"
+            :parameters="[{key: 'owner', value: '0x262bab6a90aa1741390c4a3ec58855c81d9728e1'}, {key: 'operator', value: '0x262bab6a90aa1741390c4a3ec58855c81d9728e1'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="isRefundApplied"
+            className="roomNightCustomer"
+            functionName="isRefundApplied"
+            :parameters="[{key: 'rnid', value: '1'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="roomNight"
+            className="roomNightCustomer"
+            functionName="roomNight"
+            :parameters="[{key: 'rnid', value: '1'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="roomNightsOfOwner"
+            className="roomNightCustomer"
+            functionName="roomNightsOfOwner"
+            :parameters="[{key: 'from', value: 0}, {key: 'limit', value: 10}, {key: 'isVendor', value: 'false', type: 'boolean'}]">
+        </trip-debugger>
     </div>
 </template>
 <script>
--- src/pages/room-night-customer/.write.vue
+++ src/pages/room-night-customer/.write.vue
@@ -1,10 +1,52 @@
 <template>
     <div>
         <trip-debugger 
-            title="updateBaseTokenURI"
+            title="safeTransferFrom"
             className="roomNightCustomer"
-            functionName="updateBaseTokenURI"
-            :parameters="[{key: 'uri', value: 'test'}]">
+            functionName="safeTransferFrom"
+            :parameters="[{key: 'from', value: 0}, {key: 'to', value: 10}, {key: 'tokenId', value: 1}, {key: 'data', value: ''}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="transferFrom"
+            className="roomNightCustomer"
+            functionName="transferFrom"
+            :parameters="[{key: 'from', value: 0}, {key: 'to', value: 10}, {key: 'tokenId', value: 1}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="transferFromInBatch"
+            className="roomNightCustomer"
+            functionName="transferFromInBatch"
+            :parameters="[{key: 'from', value: 0}, {key: 'to', value: 10}, {key: 'tokenIds', value: '1', type: 'array'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="approve"
+            className="roomNightCustomer"
+            functionName="approve"
+            :parameters="[{key: 'approved', value: ''}, {key: 'tokenId', value: 1}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="setApprovalForAll"
+            className="roomNightCustomer"
+            functionName="setApprovalForAll"
+            :parameters="[{key: 'operator', value: ''}, {key: 'approved', value: 'true', type: 'boolean'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="buyInBatch"
+            className="roomNightCustomer"
+            functionName="buyInBatch"
+            :parameters="[{key: 'vendorId', value: '2'}, {key: 'rpid', value: 2}, {key: 'dates', value: '', type: 'array'}, {key: 'token', value: 2}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="applyRefund"
+            className="roomNightCustomer"
+            functionName="applyRefund"
+            :parameters="[{key: 'rnid', value: '2'}, {key: 'isRefund', value: 'true', type: 'boolean'}]">
+        </trip-debugger>
+        <trip-debugger 
+            title="refund"
+            className="roomNightCustomer"
+            functionName="refund"
+            :parameters="[{key: 'rnid', value: '2'}]">
         </trip-debugger>
     </div>
 </template>
